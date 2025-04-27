70c4c2bc0a3631ed6a4177430ce1cea4566763ff
abubakkerdev <abubakkerdev@gmail.com>
2025-04-27 14:48:51 +0600

    token change

--- app/utils/cloudinary.js
+++ app/utils/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "deeb1w2kn",
-  api_key: "818769823925831",
-  api_secret: "3yaxpDantUQDPjPjZ1xML-5ZvKs",
+  api_key: "888885344686199",
+  api_secret: "n5qPUlby8pL1sJ5N-xKlv3SXEkM",
 });
 
 module.exports = cloudinary;
